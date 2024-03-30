364c6bbbe7c21a3592027a6de7b705d8567e51e5
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2024-03-30 19:53:21 +0100

    remove

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,150 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 640px)");
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="px-4 md:px-[100px]">
+      <Header isAboveMediumScreens={isAboveMediumScreens} />
+      <Main isAboveMediumScreens={isAboveMediumScreens} />
+    </div>
+  );
+}
+
+function Header({ isAboveMediumScreens }) {
+  return (
+    <header>
+      <div className="flex justify-between items-center mt-5 md:relative">
+        <a>
+          <Image
+            alt="area-finder-logo"
+            width={92}
+            height={29}
+            src="/logo.svg"
+          />
+        </a>
+        <div className="sm:flex items-center z-10">
+          {isAboveMediumScreens ? <p>Welcome!</p> : ""}
+          <Image
+            className="rounded-full border-2 border-[#fff] w-8 h-8 object-cover"
+            src="/images/user.jpeg"
+            alt="user image"
+            width={36}
+            height={36}
+          />
         </div>
       </div>
+    </header>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+function Main({ isAboveMediumScreens }) {
+  return (
+    <div className="sm:grid grid-cols-2">
+      <div className="mt-[195px] md:mt-[217px]  lg:mr-[177px] text-[#1e1e1e] mb-[245px] lg:max-w-[350px]">
+        <p className="text-[40px] font-bold leading-[48px]">
+          Find a place you will love to live!
+        </p>
+        <p className="text-base font-normal leading-[25.6px] mb-6">
+          See through the lenses of people who have lived or visited the
+          neighbourhood you might have in mind.
+        </p>
+
+        <form>
+          <div className="w-full max-w-md relative flex items-center text-[#484851]">
+            <Image
+              className="absolute ml-3 pointer-events-none"
+              width={16}
+              height={16}
+              src="/msearch.svg"
+              alt="search icon"
+            />
+            <input
+              name="search"
+              autoComplete="off"
+              aria-label="Enter Address"
+              className="w-full focus:border-[#5378F6] pr-3 pl-10 rounded-lg py-4 px-3 bg-[#F3F7FE] text-[#101012] text-sm border border-[#d4dcf1]"
+              placeholder="Enter Address"
+            />
+            <Image
+              className="absolute right-4"
+              width={16}
+              height={16}
+              src="/mcancel.svg"
+              alt="cancel icon"
+            />
+          </div>
+        </form>
+        <button className="bg-[#3366FF] px-10 py-4 uppercase rounded-[6px] text-sm font-medium  text-[#FFFFFF] mt-2">
+          Search
+        </button>
+      </div>
+      {isAboveMediumScreens ? <ReviewCollection /> : ""}
+    </div>
+  );
+}
+
+function ReviewCollection() {
+  return (
+    <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-6 sm:ml-20 md:absolute top-[-44px] right-[100px]">
+      <ReviewCard issue="/traffic.svg" />
+      <ReviewCard issue="/traffic.svg" />
+      <ReviewCard issue="/network.svg" />
+      <ReviewCard issue="/traffic.svg" />
+      <ReviewCard issue="/power.svg" />
+      <ReviewCard issue="/water.svg" />
+      <ReviewCard issue="/security.svg" />
+      <ReviewCard issue="/road.svg" />
+    </div>
+  );
+}
+
+function ReviewCard({ issue }) {
+  return (
+    <div className="w-[239px] bg-pink-500 rounded-[15px] p-4">
+      <div className="flex justify-between mb-1">
+        <div className="flex gap-1">
+          <Image
+            className="rounded-full object-cover w-6 h-6"
+            src="/images/user-rev.jpeg"
+            alt="client image"
+            height={25}
+            width={25}
+          />
+          <div>
+            <p className="text-xs font-medium leading-[14.52px]">James T.</p>
+            <p className="text-[8px] font-light leading-[9.68px]">
+              5 months ago
+            </p>
+          </div>
+        </div>
+        <div>
+          <p className="text-[11px] font-medium leading-[15.4px]">
+            Ikate,Lekki
+          </p>
+          <Image
+            src="/multistar.svg"
+            alt="rating star"
+            width={60}
+            height={12}
+          />
+        </div>
+      </div>
+      <p className="text-[14px] font-normal leading-[19.6px] mb-1">
+        There is no stable electricity. The roads are fairly good and there is a
+        sense of community. The drainage system is poor and most residents
+        litter their surroundings. There are several grocery stores and
+        Supermarkets.
+      </p>
+      <div className="flex justify-between items-center">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/revicon.svg"
+          width={118}
+          height={18}
+          alt="review reactions"
         />
+        <Image src={issue} width={51} height={14} alt="issue" />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
---
+++ pages/reviews.js
@@ -0,0 +1,329 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import Image from "next/image";
+
+export default function AllReview() {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 500px)");
+  return (
+    <div>
+      <Header isAboveMediumScreens={isAboveMediumScreens} />
+      <MainReviews isAboveMediumScreens={isAboveMediumScreens} />
+    </div>
+  );
+}
+
+function Header({ isAboveMediumScreens }) {
+  return isAboveMediumScreens ? (
+    <header className="px-[100px] py-4 bg-[#F2F6FD]">
+      <div className="grid grid-cols-[92px_1fr_125px] content-center mb-4">
+        <a>
+          <Image
+            alt="area-finder-logo"
+            width={92}
+            height={29}
+            src="/logo.svg"
+            className="my-[10.5px]"
+          />
+        </a>
+        <form>
+          <div className="ml-10 mr-[8rem] relative flex items-center text-[#484851]">
+            <Image
+              className="absolute ml-3 pointer-events-none"
+              width={16}
+              height={16}
+              src="/search.svg"
+              alt="search icon"
+            />
+            <input
+              name="search"
+              autoComplete="off"
+              aria-label="Enter Address"
+              className="w-full focus:border-[#5378F6] pr-3 pl-10 rounded-lg py-4 px-3 bg-[#F3F7FE] text-[#101012] text-sm border border-[#d4dcf1]"
+              placeholder="Enter Address"
+            />
+            <Image
+              className="absolute right-4 pointer-events-none"
+              width={16}
+              height={16}
+              src="/cancel.svg"
+              alt="cancel icon"
+            />
+          </div>
+        </form>
+        <div className="flex items-center gap-3">
+          <span>Welcome!</span>
+          <Image
+            className="my-[9px] rounded-full border-2 border-[#fff] w-8 h-8 object-cover"
+            src="/images/user.jpeg"
+            alt="user image"
+            width={32}
+            height={32}
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-[1fr_210px_repeat(2,_50px)] md:grid-rows-1 gap-4 content-center mb-4">
+        <div>
+          <h1 className="text-2xl font-medium leading-[19.2px]">
+            Bonny and Clyde Street, Ajao Estate, Lagos
+          </h1>
+          <p className="text-base leading-[24px] font-medium">
+            <span className="font-semibold">“450”</span> Reviews{" "}
+            <span className="font-normal">
+              (People are raving about the selected location)
+            </span>
+          </p>
+        </div>
+        <button className="bg-[#3366FF] h-[50px] px-10 py-4 uppercase rounded-[6px] text-sm font-medium  text-[#FFFFFF]">
+          leave a review
+        </button>
+        <Image
+          className="h-[50px]"
+          src="/bookmark.svg"
+          width={56}
+          height={50}
+          alt="bookmark"
+        />
+        <Image
+          className="h-[50px]"
+          src="/share.svg"
+          width={56}
+          height={50}
+          alt="share"
+        />
+      </div>
+
+      <div className="flex flex-wrap items-center gap-2 text-sm font-normal leading-[16.1px]">
+        <span className="cat-label">Schools</span>
+        <span className="cat-label">Hospitals</span>
+        <span className="cat-label">Resort Park</span>
+        <span className="cat-label">Shopping Malls</span>
+        <span className="cat-label">Airport</span>
+        <span className="cat-label">Train Station</span>
+        <span className="cat-label">Nightlife</span>
+        <span className="cat-label">Public Wifi</span>
+        <span className="cat-label">Parking Lot</span>
+        <span className="cat-label">Security</span>
+        <span className="cat-label">Public Transport</span>
+        <span className="cat-label">Bus Station</span>
+        <span className="cat-label">Quiet</span>
+        <Image
+          src="/chevron-right.svg"
+          alt="click more"
+          width={24}
+          height={24}
+        />
+      </div>
+    </header>
+  ) : (
+    <header className="mb-4 px-4">
+      <div className="mb-2 flex items-center justify-between">
+        <a>
+          <Image
+            alt="area-finder-logo"
+            width={92}
+            height={29}
+            src="/logo.svg"
+            className="my-[10.5px]"
+          />
+        </a>
+        <Image
+          className="my-[9px] rounded-full h-8 w-8 object-cover"
+          src="/images/user.jpeg"
+          alt="user image"
+          width={32}
+          height={32}
+        />
+      </div>
+      <form>
+        <div className="w-full max-w-md relative flex items-center text-[#484851]">
+          <Image
+            className="absolute ml-3 pointer-events-none"
+            width={16}
+            height={16}
+            src="/msearch.svg"
+            alt="search icon"
+          />
+          <input
+            name="search"
+            autoComplete="off"
+            aria-label="Enter Address"
+            className="w-full focus:border-[#5378F6] pr-3 pl-10 rounded-lg py-4 px-3 bg-[#F3F7FE] text-[#101012] text-sm border border-[#d4dcf1] [grid-area:2_/_1_/_3_/_3]"
+            placeholder="Enter Address"
+          />
+          <Image
+            className="absolute right-4"
+            width={16}
+            height={16}
+            src="/mcancel.svg"
+            alt="cancel icon"
+          />
+        </div>
+      </form>
+      <div className="mb-4">
+        <h1 className="text-base font-medium leading-[19.2px] mb-1">
+          Bonny and Clyde Street, Ajao Estate, Lagos
+        </h1>
+        <p className="text-xs font-medium leading-[18px]">
+          <span className="font-semibold">“450”</span> Reviews{" "}
+          <span className="font-normal">
+            (People are raving about the selected location)
+          </span>
+        </p>
+      </div>
+      <div className="flex flex-wrap gap-2 mb-4">
+        <span className="cat-label">Schools</span>
+        <span className="cat-label">Hospitals</span>
+        <span className="cat-label">Airport</span>
+        <span className="cat-label">Gym</span>
+        <span className="cat-label">Park</span>
+        <span className="cat-label">Wifi</span>
+      </div>
+      <div className="flex flex-wrap gap-4">
+        <button className="bg-[#3366FF] px-10 py-4 uppercase rounded-[6px] text-sm font-medium  text-[#FFFFFF]">
+          leave a review
+        </button>
+        <Image src="/bookmark.svg" width={56} height={50} alt="bookmark" />
+        <Image src="/share.svg" width={56} height={50} alt="share" />
+      </div>
+    </header>
+  );
+}
+
+function MainReviews({ isAboveMediumScreens }) {
+  return (
+    <section>
+      {isAboveMediumScreens ? (
+        <div className="flex gap-4 px-[100px] py-4">
+          <ReviewList />
+          <div className="grid grid-cols-[repeat(2,_235px)] grid-rows-[repeat(2,_224px)] gap-2">
+            <Image
+              className="rounded-lg h-full w-full object-cover"
+              src="/images/img-1.jpeg"
+              width={235}
+              height={224}
+              alt="area-1"
+            />
+            <Image
+              className="rounded-lg h-full w-full object-cover"
+              src="/images/img-2.jpeg"
+              width={235}
+              height={224}
+              alt="area-2"
+            />
+            <Image
+              className="rounded-lg h-full w-full object-cover"
+              src="/images/img-3.jpeg"
+              width={235}
+              height={224}
+              alt="area-3"
+            />
+            <Image
+              className="rounded-lg h-full w-full object-cover"
+              src="/images/img-4.jpeg"
+              width={235}
+              height={224}
+              alt="area-4"
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="px-4">
+          <div className="grid grid-cols-[175px_160px] auto-rows-[110px] gap-2 mb-6">
+            <Image
+              className="rounded-lg row-span-2 h-full w-full  object-cover"
+              src="/images/img-1.jpeg"
+              width={235}
+              height={224}
+              alt="area-1"
+            />
+            <Image
+              className="rounded-lg h-full w-full object-cover"
+              src="/images/img-2.jpeg"
+              width={235}
+              height={224}
+              alt="area-2"
+            />
+            <Image
+              className="rounded-lg h-full w-full object-cover"
+              src="/images/img-3.jpeg"
+              width={235}
+              height={224}
+              alt="area-3"
+            />
+          </div>
+          <ReviewList />
+        </div>
+      )}
+    </section>
+  );
+}
+
+function ReviewList() {
+  return (
+    <ul>
+      <li>
+        <Review />
+      </li>
+      <li>
+        <Review />
+      </li>
+      <li>
+        <Review />
+      </li>
+      <li>
+        <Review />
+      </li>
+      <li>
+        <Review />
+      </li>
+    </ul>
+  );
+}
+function Review() {
+  return (
+    <div className="pb-4 border-b border-[#D9D9D9] mb-4">
+      <div className="flex items-center justify-between mb-2">
+        <div className="flex items-center gap-2">
+          <Image
+            className="rounded-full object-cover w-6 h-6"
+            src="/images/user-rev.jpeg"
+            width={24}
+            height={24}
+            alt="reviewer image"
+          />
+          <span>James T.</span>
+          <span>5 months ago</span>
+        </div>
+        <div className="flex items-center">
+          {" "}
+          <Image
+            src="/star.svg"
+            width={12}
+            height={12}
+            alt="star rating icon"
+          />{" "}
+          <span className="ml-[2px]">4.0</span>
+        </div>
+      </div>
+      <p className="text-[16px] font-normal leading-[24px]">
+        There is no stable electricity. The roads are fairly good and there is a
+        sense of community. The drainage system is poor and most residents
+        litter their surroundings. There are several grocery stores and
+        Supermarkets.
+      </p>
+      <div className="flex items-center gap-8 text-[#0D2159]">
+        <div className="flex items-center">
+          <Image src="/like.svg" width={24} height={24} alt="like icon" />
+          <span>1000</span>
+        </div>
+        <div className="flex items-center">
+          <Image src="/dislike.svg" width={24} height={24} alt="dislike icon" />
+          <span>24</span>
+        </div>
+        <div className="flex items-center">
+          <Image src="/comment.svg" width={24} height={24} alt="comment icon" />
+          <span>24</span>
+        </div>
+      </div>
+    </div>
+  );
+}
