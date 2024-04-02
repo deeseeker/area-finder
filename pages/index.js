c4a556729d857887993045170245c1892467f3cc
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2024-04-02 23:16:40 +0100

    new update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import VerticalSlider from "@/components/VerticalSlider";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
 export default function Home() {
@@ -42,6 +43,7 @@ function Header({ isAboveMediumScreens }) {
 
 function Main({ isAboveMediumScreens }) {
   const [searchText, setSearchText] = useState("");
+  const router = useRouter();
 
   const handleInputChange = (e) => {
     setSearchText(e.target.value);
@@ -96,7 +98,10 @@ function Main({ isAboveMediumScreens }) {
             )}
           </div>
         </form>
-        <button className="bg-[#3366FF] active:bg-[#061E66] hover:bg-[#2952CC] px-10 py-4 uppercase rounded-[6px] text-sm font-medium  text-[#FFFFFF] mt-2">
+        <button
+          onClick={() => router.push("/reviews")}
+          className="bg-[#3366FF] active:bg-[#061E66] hover:bg-[#2952CC] px-10 py-4 uppercase rounded-[6px] text-sm font-medium  text-[#FFFFFF] mt-2"
+        >
           Search
         </button>
       </div>
